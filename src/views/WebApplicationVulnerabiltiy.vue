61fc5bebc383d870785f0cb2d5dde5dc571c9c6a
Thinzar Aung <116476248+ThinzarAung24@users.noreply.github.com>
2023-01-29 16:35:48 +0630

    Update WebApplicationVulnerabiltiy.vue

--- src/views/WebApplicationVulnerabiltiy.vue
+++ src/views/WebApplicationVulnerabiltiy.vue
@@ -7,7 +7,7 @@
           <!-- <h2>RESTAURNANT ORDER SYSTEM</h2> -->
           <div class="waviy">
             <span style="--i: 1"
-              >Effective Web Application Vulnerability Testing System using XSS_SQL_Scanning_Algorithm</span>
+              ></span>
           </div>
         </div>
       </div>
